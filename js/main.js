2ad60d8e7f23680c1dd1e5cdacd3228e0eedb6ed
musuko <100979661+musuko@users.noreply.github.com>
2026-02-17 21:04:46 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+window.addEventListener('load', () => {
+    loadStopsFromZip();
 
+    // 初回
+    setTimeout(updateBusPositions, 5000);
+
+    // 定期更新
+    setInterval(updateBusPositions, 15000);
+});
