7ffa1fffd7783b8b07c7e56ca2a6b8a5e9784e2b
musuko <100979661+musuko@users.noreply.github.com>
2026-02-17 21:03:19 +0900

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,13 @@
+// mapをグローバル化（重要）
+window.map = L.map('map').setView([34.3976, 132.4754], 12);
 
+// 背景
+L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+  attribution: "<a href='https://maps.gsi.go.jp' target='_blank'>地理院タイル</a>"
+}).addTo(map);
+
+// スケール
+L.control.scale({
+  position: "bottomright",
+  imperial: false
+}).addTo(map);
