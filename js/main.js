fc0e50980282a13754c94b38ba1f642f2386bf55
musuko <100979661+musuko@users.noreply.github.com>
2026-02-19 11:14:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ window.addEventListener('load', () => {
     loadStopsFromZip();
 
     // 初回
-    setTimeout(updateBusPositions, 5000);
+    setTimeout(updateBusPositions, 15000);
 
     // 定期更新
-    setInterval(updateBusPositions, 60000);
+    setInterval(updateBusPositions, 15000);
 
      startGeolocation(); // ← これ追加
 });
