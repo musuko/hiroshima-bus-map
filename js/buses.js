3515f1def1295906ef02137c8a02b7a4cbb6ab83
musuko <100979661+musuko@users.noreply.github.com>
2026-02-20 12:39:23 +0900

    Update buses.js

--- js/buses.js
+++ js/buses.js
@@ -16,8 +16,8 @@ window.updateBusPositions = async function() {
 
         const busIcon = L.icon({
             iconUrl: './busimg/green.png',
-            iconSize: [80, 80],
-            iconAnchor: [40, 40],
+            iconSize: [20, 20],
+            iconAnchor: [10, 10],
             popupAnchor: [0, -20]
         });
 
