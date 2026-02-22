ca5a99fc33de39481e005ac359e8d4b7aa4cb2c9
musuko <100979661+musuko@users.noreply.github.com>
2026-02-22 10:36:21 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,18 @@
-// js/config.js (新規作成イメージ)
 const BUS_COMPANIES = [
     {
         id: 'hiroden',
         name: '広電バス',
         staticPath: './info/hiroden/',
-        realtimeUrl: 'https://hiroden-api.vercel.app/api/get-bus',
+        // id=8 を指定
+        realtimeUrl: 'https://xxx.vercel.app/api/get-bus?id=8',
         active: true
     },
     {
         id: 'hirobus',
         name: '広島バス',
         staticPath: './info/hirobus/',
-        realtimeUrl: 'https://hirobus-api.vercel.app/api/get-bus', // 仮
+        // id=9 を指定
+        realtimeUrl: 'https://xxx.vercel.app/api/get-bus?id=9',
         active: true
     }
-    // 今後ここに追加するだけで全機能が対応する
 ];
